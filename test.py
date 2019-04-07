50e55a4229b4bcf71f5d1dea564620903b46642c
DESKTOP-R0GMT8H\Dan <danrbritt@gmail.com>
2019-04-07 17:34:13 -0500

    Added code for custom cparams

--- test.py
+++ test.py
@@ -51,6 +51,11 @@ print(df.memory_usage().sum())
 
 ct = bcolz.ctable.fromdataframe(df)
 
+# Can use this code instead to override default compression params
+# It should throw an error, because a compression level of 11 is not valid for blosclz compression method, but it doesn't
+# test_compression_params = bcolz.cparams(clevel=11, cname='blosclz')
+# ct = bcolz.ctable.fromdataframe(df, cparams=test_compression_params)
+
 print('ctable uncomressed size (btyes)')
 print(ct.nbytes)
 print('ctable compressed size (bytes)')
